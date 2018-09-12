d5c1dee8a214384c648234220adf972efe5abbae
gentleimp <42977930+gentleimp@users.noreply.github.com>
2018-09-12 22:57:58 +0500

    Решение Задача 1. Виджет погоды
    Решение Задача 1. Виджет погоды

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,9 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', true);
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
-}
+
+request.addEventListener('load', function() {	
+ 	const response = JSON.parse(request.responseText);
+  	setData(response);	
+});
+
