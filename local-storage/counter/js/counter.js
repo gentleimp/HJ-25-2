44ce03683484796229dccaab64148d1b017ae760
gentleimp <42977930+gentleimp@users.noreply.github.com>
2018-09-24 20:50:48 +0500

    Решение Задача 1. Персональный счетчик
    Решение Задача 1. Персональный счетчик

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const counter = document.querySelector('#counter'),
+	increment = document.querySelector('#increment'),
+	decrement = document.querySelector('#decrement'),
+	reset = document.querySelector('#reset'),
+	wrapBtns = document.querySelector('.wrap-btns');
+
+let count = localStorage.getItem('counter') || 0;
+
+function writeCounter() {
+  counter.innerHTML = count;
+  localStorage.setItem('counter', count);
+}
+
+function changeCounter() {
+	switch (event.target) {
+		case increment:
+			count++;
+			break;
+		case decrement:
+			if (count > 0) {
+				count--;
+			}
+			break;
+		case reset:
+			count = 0;
+	}	
+  	writeCounter();
+}
+
+wrapBtns.addEventListener('click', changeCounter);
+writeCounter();
+
