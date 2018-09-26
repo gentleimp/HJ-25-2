2ca556210544e57cd6eda2a3bc02e6e9140216f7
gentleimp <42977930+gentleimp@users.noreply.github.com>
2018-09-26 21:13:16 +0500

    Решение Задача 1. Шарики
    Решение Задача 1. Шарики

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse'),
+  body = document.querySelector('body');
+
+function sendClick(event) {
+  let coordinates = {x: client.screenX, y: client.screenY};
+
+   connection.send(JSON.stringify(coordinates));   
+}
+
+document.addEventListener('click', sendClick);
+
+connection.addEventListener('open', showBubbles(connection));
+
+connection.addEventListener('error', error => {
+  console.log(`Произошла ошибка: ${error.data}`);
+});
+
+window.addEventListener('beforeunload', () => {
+  connection.close(1000);
+});
