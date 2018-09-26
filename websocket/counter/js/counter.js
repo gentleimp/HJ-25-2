ef4270efbf7bae1414d487c139811983139a8877
gentleimp <42977930+gentleimp@users.noreply.github.com>
2018-09-26 21:31:53 +0500

    Решение Задача 2. Счетчик открытых соединений
    Решение Задача 2. Счетчик открытых соединений

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter'),
+  cnctsCounter = document.querySelector('.counter'),
+  errsCounter = document.querySelector('output.errors');
+
+document.addEventListener('load', () => {
+  connection.addEventListener('open', )
+});
+
+connection.addEventListener('message', event => {
+  let message = JSON.parse(event.data);
+  cnctsCounter.innerHTML = message.connections;
+  errsCounter.innerHTML = message.errors;
+})
+
+window.addEventListener('beforeunload', () => {
+  connection.onclose = function () {};
+  connection.close(1000);
+});
+
+connection.addEventListener('error', error => {
+console.log(`Произошла ошибка: ${error.data}`);
+});
