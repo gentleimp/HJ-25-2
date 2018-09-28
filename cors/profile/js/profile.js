626a4cc9e5780f4697ab66e37b7ec1969b06b79c
gentleimp <42977930+gentleimp@users.noreply.github.com>
2018-09-29 01:05:13 +0500

    Решение Задача 2. Профиль пользователя
    Решение Задача 2. Профиль пользователя

--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -1,1 +1,33 @@
 'use strict';
+const content = document.querySelector('.content'),
+  badge = document.querySelector('.content [data-technologies]');
+
+function addScript(src, getData) {  
+  return new Promise((getData) => { 
+    
+    const script = document.createElement("script");
+    script.src = src;
+    document.body.appendChild(script);    
+  });
+}
+
+function getUserData(data) {
+  let userId = data.id;  
+  addScript(`https://neto-api.herokuapp.com/profile/${userId}/technologies?callback=getTech`, getTech);
+  document.querySelector('.card [data-name]').innerHTML = data.name;
+  document.querySelector('.card [data-description]').innerHTML = data.description;
+  document.querySelector('.card [data-position]').innerHTML = data.position;
+  document.querySelector('.card [data-pic]').setAttribute('src', data.pic);
+}
+
+addScript('https://neto-api.herokuapp.com/profile/me?callback=getUserData', getUserData);
+
+function getTech(data) {
+  for (let tech of data) {
+    let elem = document.createElement("span");    
+    elem.classList.add('devicons');
+    elem.classList.add(`devicons-${tech}`);
+    badge.appendChild(elem);
+  }
+  content.style.display = 'initial';
+}
