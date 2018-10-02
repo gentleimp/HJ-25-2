0e2083fcb7264d508e03e69ff3b64ac622d64402
gentleimp <42977930+gentleimp@users.noreply.github.com>
2018-10-02 23:26:32 +0500

    Решение Задача 1. Генератор звёздного неба
    Решение Задача 1. Генератор звёздного неба

---
+++ canvas/star-field/js/star.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const canvas = document.querySelector('canvas');
+
+let ctx = canvas.getContext('2d');
+let colorVrts = ['#ffffff', '#ffe9c4', '#d4fbff'];
+
+function skyGenerator() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  let amtOfStars = Math.round(Math.random()*200) + 200;
+  for (let i = 0; i < amtOfStars; i++) {
+    let size = Math.random()*1.1;    
+    let color = colorVrts[Math.round(Math.random()*2)];
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    let opacity = Math.random()*0.2 + 0.8;
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.globalAlpha = opacity;
+    ctx.arc(x, y, size, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+canvas.addEventListener('click', skyGenerator);
