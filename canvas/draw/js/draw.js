6e807970072748e84d19aa68a52c424b3f18d3dc
gentleimp <42977930+gentleimp@users.noreply.github.com>
2018-10-03 00:36:09 +0500

    Решение Задача 2. Рисование в браузере
    Решение Задача 2. Рисование в браузере

--- canvas/draw/js/draw.js
+++ canvas/draw/js/draw.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const canvas = document.querySelector('#draw'),
+  ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', (e) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+const toneMin = 0, toneMax = 359,
+  thicknessMin = 5, thicknessMax = 100; 
+  
+let tone = 0, toneMode, 
+  thickness = 100, thicknessMode = -1;
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+function lineDrawing(e) {    
+    ctx.lineTo(e.clientX, e.clientY);  
+
+    if (thickness == thicknessMin) {
+      thicknessMode = 1;
+    } else if (thickness == thicknessMax) {
+      thicknessMode = -1;
+    }
+    thickness += thicknessMode;
+    ctx.lineWidth = thickness;
+    
+    if (toneMode) {
+      tone += 1;
+    } else tone -= 1;  
+    ctx.strokeStyle = `hsl(${tone}, 100%, 50%)`;  
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  toneMode = e.shiftKey; 
+
+  ctx.beginPath();
+  ctx.moveTo(e.clientX, e.clientY);
+  e.preventDefault();
+  canvas.addEventListener('mousemove', lineDrawing);
+});
+
+canvas.addEventListener('mouseup', () => {
+  canvas.removeEventListener('mousemove', lineDrawing);
+});
+
+canvas.addEventListener('dblclick', () => { 
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+canvas.addEventListener("mouseleave", () => {
+  canvas.removeEventListener('mousemove', lineDrawing);
+});
+
