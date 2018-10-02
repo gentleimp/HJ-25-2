1d447b494fe3ae3b7570f7131eaee69963d3c85a
gentleimp <42977930+gentleimp@users.noreply.github.com>
2018-10-03 00:35:29 +0500

    Решение Задача 2. Рисование в браузере
    Решение Задача 2. Рисование в браузере

--- canvas/star-field/js/star.js
+++ canvas/star-field/js/star.js
@@ -2,8 +2,8 @@
 
 const canvas = document.querySelector('canvas');
 
-let ctx = canvas.getContext('2d');
-let colorVrts = ['#ffffff', '#ffe9c4', '#d4fbff'];
+const ctx = canvas.getContext('2d');
+const colorVrts = ['#ffffff', '#ffe9c4', '#d4fbff'];
 
 function skyGenerator() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
